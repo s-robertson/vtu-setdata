7758fa2468489d815b99d69a7b895a95b1e45e73
Scott Robertson <srobertson@benevity.com>
2018-04-12 08:12:00 -0700

    Use dev branch of vue-test-utils

--- tests/unit/HelloWorld.spec.js
+++ tests/unit/HelloWorld.spec.js
@@ -1,4 +1,4 @@
-import { shallow } from '@vue/test-utils'
+import { shallow } from '@vue/test-utils/packages/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
